f1a2cab760037f2a0f73e201efe8a8c3f35d8238
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2023-09-21 12:40:38 +0530

    Time: 0 ms (100%), Space: 8.6 MB (10.29%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>ump;
-        for(int i = 0;i<arr.size();i++)
-            ump[arr[i]]++;
-        unordered_map<int,int>::iterator it=ump.begin();
-        unordered_map<int,int>::iterator it1=ump.begin();
-        // int y=it->second;
-        while(it!=ump.end())
-        {
-            it1=it;
-            it1++;
-            while(it1!=ump.end())
-            {
-                if(it->second==it1->second)
-                return false;
-                it1++;
-            }
-            it++;
+        unordered_map<int,int> map;
+        for(int num : arr) map[num]++;
+        vector<int> occurrence;
+        for(const auto& pair:map){
+            occurrence.push_back(pair.second);
         }
-        return true;
+        set<int> s(occurrence.begin(),occurrence.end());
+        if (s.size()==occurrence.size()) return true;
+        else return false;
     }
 };
