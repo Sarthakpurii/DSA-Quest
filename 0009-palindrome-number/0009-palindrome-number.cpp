b1397ce1a2d68f6d5b366a556b300e0438249c68
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2023-09-21 13:47:11 +0530

    Time: 3 ms (93.97%), Space: 6.2 MB (14.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x>=0){
-            int copy=x;
-            long n=0;
-            while(x!=0){
-                n=(n*10)+(x%10);
-                x/=10;
+        if (x<0) return false;
+        int copy=x;
+        long n=0;
+        while(x!=0){
+            n=(n*10)+(x%10);
+            x/=10;
             }
-        if (copy==n) return true;
-        }
-        return false;
+        return (copy==n);
     }
 };
