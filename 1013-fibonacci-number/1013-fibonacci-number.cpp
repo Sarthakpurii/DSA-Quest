11a0e51b9b0d020b5d8efd0e062e6af2643bbf99
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2023-09-22 08:30:59 +0530

    Time: 0 ms (100%), Space: 6.3 MB (10.57%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,28 @@
 class Solution {
 public:
+    vector<vector<int>> MatrixMul(vector<vector<int>> a,vector<vector<int>> b){
+        vector<vector<int>> ans={{0,0},{0,0}};
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                for(int k=0;k<2;k++){
+                    ans[i][j]+=a[i][k]*b[k][j];
+                }
+            }
+        }
+        return ans;
+    }
+
+    vector<vector<int>> ExpoMul(int n){
+        vector<vector<int>> ans ={{1,0},{0,1}},x = {{1,1},{1,0}};
+        while(n){
+            if (n&1) ans=MatrixMul(ans,x);
+            x=MatrixMul(x,x);
+            n/=2;
+        }
+        return ans;
+    }
     int fib(int n) {
-        if( n==1) return 1;
-        else if (n==0) return 0;
-        return fib(n-1)+fib(n-2);
+        vector<vector<int>> ans=ExpoMul(n);
+        return ans[1][0];
     }
 };
