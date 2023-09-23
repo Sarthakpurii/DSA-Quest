b66668ce41236de6b87d8ecfa506a17fe2cc0ab5
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2023-09-24 00:24:52 +0530

    Time: 27 ms (15.96%), Space: 19.5 MB (16.34%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        //Approach 1
+
+        // int j=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if (nums[j]==0){
+        //         nums.erase(nums.begin()+j);
+        //         nums.push_back(0);
+        //         j--;
+        //     }
+        //     j++;
+        // }
+
+        // Approach 2
+
+        int zero=0,nonzero=0;
+        while(nonzero<nums.size()){
+            if(nums[nonzero]!=0){
+                swap(nums[zero],nums[nonzero]);
+                zero++;
+            }
+            cout<<nonzero;
+            nonzero++;
+        }
+    }
+};
