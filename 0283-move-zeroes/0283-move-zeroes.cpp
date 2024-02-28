8486abfea044a54ddd59f358f1fa9168ee7d883d
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-02-29 03:21:19 +0530

    Time: 10 ms (94.54%), Space: 21.5 MB (49.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,27 +3,15 @@ public:
     void moveZeroes(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        //Approach 1
-
-        // int j=0;
-        // for(int i=0;i<nums.size();i++){
-        //     if (nums[j]==0){
-        //         nums.erase(nums.begin()+j);
-        //         nums.push_back(0);
-        //         j--;
-        //     }
-        //     j++;
-        // }
-
-        // Approach 2
-
-        int zero=0,nonzero=0;
-        while(nonzero<nums.size()){
-            if(nums[nonzero]!=0){
-                swap(nums[zero],nums[nonzero]);
-                zero++;
+        int pointer=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[pointer]=nums[i];
+                pointer++;
             }
-            nonzero++;
+        }
+        for(pointer;pointer<nums.size();pointer++){
+            nums[pointer]=0;
         }
     }
 };
