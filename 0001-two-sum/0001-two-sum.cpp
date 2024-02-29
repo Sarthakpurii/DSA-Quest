4eadc4f000bf9aa1a1426fd5d039c3c82170925c
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-01 02:10:52 +0530

    Time: 0 ms (100%), Space: 14.1 MB (19.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,10 +3,15 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        for (int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if (nums[i]+nums[j]==target) return {i,j};
+
+        int n=nums.size();
+        unordered_map<int,int> mapped;
+        for(int i=0;i<n;i++){
+            int rem=target-nums[i];
+            if(mapped.find(rem)!=mapped.end()){
+                return {mapped[rem],i};
             }
+            mapped[nums[i]]=i;
         }
         return {};
     }
