8286de9c81efe91c583260025802e0fc046edc4e
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-01 03:30:37 +0530

    Time: 5 ms (15.02%), Space: 10 MB (23.01%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,13 +4,26 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        vector<int> counter(3,0);
-        for(int i=0;i<nums.size();i++){
-            counter[nums[i]]++;
-        }
-        int index=0;
-        for(int i=0;i<3;i++){
-            while(counter[i]-- > 0) nums[index++]=i;
-        }
+        // vector<int> counter(3,0);
+        // for(int i=0;i<nums.size();i++){
+        //     counter[nums[i]]++;
+        // }
+        // int index=0;
+        // for(int i=0;i<3;i++){
+        //     while(counter[i]-- > 0) nums[index++]=i;
+        // }
+
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;}
+            else if(nums[mid]==2){
+                swap(nums[high],nums[mid]);
+                high--;
+            }
+            else mid++;
+            }
     }
 };
