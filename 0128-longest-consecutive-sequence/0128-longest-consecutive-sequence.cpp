1320d85a5e0223bd49c41cad307bc65fe0a59aeb
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-02 02:56:03 +0530

    Time: 78 ms (76.99%), Space: 73.8 MB (52.55%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,21 +4,40 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        
+        // nlogn approach as insertion in set takes nlogn on avg
+        // if(nums.size()==0) return 0;
+
+        // set<int> sett(nums.begin(),nums.end());
+        // int solution=0,temp=0;
+
+        // auto it=sett.begin();
+        // for(int n:sett){
+        //     advance(it, 1);
+        //     if(it!=sett.end() && *it-n==1){
+        //         temp++;
+        //     }
+        //     else{
+        //         solution=max(temp+1,solution);
+        //         temp=0;
+        //     }
+        // }
+        // return solution;
+
+
+        // using unordered set O(n) appraoch as avg insertion time is O(n)
+
         if(nums.size()==0) return 0;
 
-        set<int> sett(nums.begin(),nums.end());
-        int solution=0,temp=0;
-
-        auto it=sett.begin();
+        unordered_set<int> sett(nums.begin(),nums.end());
+        int solution=0;
         for(int n:sett){
-            advance(it, 1);
-            if(it!=sett.end() && *it-n==1){
-                temp++;
-            }
-            else{
-                solution=max(temp+1,solution);
-                temp=0;
+            if(sett.find(n-1)==sett.end()){
+                int temp=1,tempNum=n+1;
+                while(sett.find(tempNum)!=sett.end()){
+                    temp++;
+                    tempNum++;
+                }
+                solution=max(solution,temp);
             }
         }
         return solution;
