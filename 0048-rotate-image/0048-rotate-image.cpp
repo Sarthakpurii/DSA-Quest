d83212e7706a5938b95275e57178a880f975c9a0
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-05 00:45:24 +0530

    Time: 0 ms (100%), Space: 8.8 MB (15.94%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        
+
         int n=matrix.size();
 
         for(int i=0;i<n;i++){
@@ -13,7 +13,7 @@ public:
         }
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++) swap(matrix[i][j],matrix[i][n-j-1]);
+            reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 };
