8cee6513c28aeb477c0963afec1e5ae4078c7bbc
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-05 14:34:16 +0530

    Time: 0 ms (100%), Space: 18.5 MB (37.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        // Create a frequency map to store the count of each element
-        unordered_map<int, int> elementCountMap;
-        
-        // Iterate through the input array to count element occurrences
-        for(int i = 0; i < nums.size(); i++) {
-            elementCountMap[nums[i]]++;
-        }
-        
-        vector<int> majorityElements;
-        int threshold = nums.size() / 3;
-        
-        // Iterate through the frequency map to identify majority elements
-        for(auto elementCountPair : elementCountMap) {
-            int element = elementCountPair.first;
-            int count = elementCountPair.second;
-            
-            // Check if the element count is greater than the threshold
-            if(count > threshold) {
-                majorityElements.push_back(element);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        // unordered_map<int,int> table;
+        // for(int i=0;i<nums.size();i++){
+        //     table[nums[i]]++;
+        // }
+        // vector<int> sol;
+        // for(auto it: table){
+        //     if (it.second>nums.size()/3) sol.push_back(it.first);
+        // }
+        // return sol;
+
+        int c1=0,c2=0,cand1,cand2;
+        vector<int> solution;
+
+        for(int i=0;i<nums.size();i++){
+            if(c1==0 && cand2!=nums[i]){
+                cand1=nums[i];
+                c1++;
+            }
+            else if(c2==0 && cand1!=nums[i]){
+                cand2=nums[i];
+                c2++;
+            }
+            else if (cand1==nums[i]) c1++;
+            else if (cand2==nums[i]) c2++;
+            else{
+                c1--;
+                c2--;
             }
         }
+
+        c1=0,c2=0;
+        for(int i=0;i<nums.size();i++){
+            if(cand1==nums[i]) c1++;
+            else if (cand2==nums[i]) c2++;
+        }
         
-        return majorityElements; 
+        if(c1>nums.size()/3) solution.push_back(cand1);
+        if(c2>nums.size()/3) solution.push_back(cand2);
+        return solution;
+        
     }
 };
