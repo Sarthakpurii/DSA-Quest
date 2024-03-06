329f971ed366ba51c89070604ebcdce6ef5ba3cc
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-06 22:32:35 +0530

    Time: 4 ms (9.64%), Space: 8.7 MB (7.43%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        if (s==goal) return true;
+        int len=s.size();
+
+        for(int i=0;i<len-1;i++){
+            s=s.substr(1,len-1)+s[0];
+            if (s==goal) return true;
+        }
+        return false;
+    }
+};
