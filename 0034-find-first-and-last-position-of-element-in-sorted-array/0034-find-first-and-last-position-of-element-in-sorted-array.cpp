d399851dd55517a922200698be69ddacb8308602
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-19 03:03:22 +0530

    Time: 0 ms (100%), Space: 16.2 MB (5.36%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        
-        if (left <= right) {
-            return {left, right};
-        } else {
-            return {-1, -1};
-        }
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int start=binarySearch(nums,target,true);
+        int end=-1;
+        if(start!=-1) end=binarySearch(nums,target,false);
+        return {start,end};
     }
-
 private:
-    int binarySearch(vector<int>& nums, int target, bool findLeft) {
-        int low = 0;
-        int high = nums.size() - 1;
-        int index = -1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                index = mid;
-                if (findLeft) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+    int binarySearch(vector<int> &nums,int target,bool isStart){
+        int low=0,high=nums.size()-1,idx=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                idx=mid;
+                if(isStart){
+                    high=mid-1;
                 }
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+                else{
+                    low=mid+1;
+                }
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
             }
         }
-        
-        return index;
+        return idx;
     }
 };
