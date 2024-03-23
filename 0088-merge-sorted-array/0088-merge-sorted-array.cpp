1945b9e52c16e4b6d970b1b38c61b86c534e0dce
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-03-23 21:32:56 +0530

    Update 0088-merge-sorted-array.cpp

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,7 +3,6 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-
         n=n-1,m=m-1;
         int k=n+m+1;
         while(n>=0){
