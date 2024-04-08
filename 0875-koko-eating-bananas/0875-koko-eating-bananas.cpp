3ee0df4a3297175cafa29463c18673b647ffee06
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-04-08 15:31:34 +0530

    Time: 26 ms (87.7%), Space: 37.6 MB (6.33%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int low=1,high=*max_element(piles.begin(),piles.end());
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long hrs=hrsReq(piles,mid);
+            // cout<<hrs<<" ";
+            if(hrs>h) low=mid+1;
+            else high=mid-1;
+        }
+        return low;
+    }
+private:
+    long hrsReq(vector<int> nums,int rate){
+        long hrs=0;
+        for(int i:nums){
+            hrs+=ceil(i/(double)rate);
+        }
+        return hrs;
+    }
+};
