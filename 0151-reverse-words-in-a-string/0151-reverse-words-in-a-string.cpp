86253944d0ec0194e75af5fd05df6b1b9c30a00d
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-04-20 05:38:37 +0530

    Time: 6 ms (47.23%), Space: 8.2 MB (90.39%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,23 +3,46 @@ public:
     string reverseWords(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        string ans;
-        int i=s.size()-1,slen=0;
-        for(i;i>-1;i--){
-            if(s[i]==' '){
-                if(slen!=0){
-                    ans+=s.substr(i+1,slen)+' ';
-                    slen=0;
-                }
+
+        int i=0,left=0,right=0,n=s.size()-1;
+        reverse(s.begin(),s.end());
+
+        while(i<=n){
+            while(i<=n && s[i]==' ')i++;
+            if(i>n) break;            //As now the i is out of bounds
+            while(i<=n && s[i]!=' '){
+                s[right++]=s[i++];
             }
-            else{
-                slen++;
-            }
+            reverse(s.begin()+left,s.begin()+right);
+            s[right++]=' ';
+            left=right;
+            i++;
+            
         }
-        if(slen!=0){
-            ans+=s.substr(i+1,slen)+' ';
-        }
-        ans.pop_back();
-        return ans;
+        s.resize(right-1);
+        return s;
+
+
+
+        // ----Takes Extra Space--- 
+
+        // string ans;
+        // int i=s.size()-1,slen=0;
+        // for(i;i>-1;i--){
+        //     if(s[i]==' '){
+        //         if(slen!=0){
+        //             ans+=s.substr(i+1,slen)+' ';
+        //             slen=0;
+        //         }
+        //     }
+        //     else{
+        //         slen++;
+        //     }
+        // }
+        // if(slen!=0){
+        //     ans+=s.substr(i+1,slen)+' ';
+        // }
+        // ans.pop_back();
+        // return ans;
     }
 };
