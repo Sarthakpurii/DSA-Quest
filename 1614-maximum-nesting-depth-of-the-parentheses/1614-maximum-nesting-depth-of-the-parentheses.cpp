8c6073e765845a6b2549fd224c0f95732d376a0f
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-05-05 20:40:26 +0530

    Time: 0 ms (100%), Space: 7.5 MB (46.32%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int temp=0,sol=0;
+        for(char c:s){
+            if(c=='('){
+                temp++;
+                sol=max(temp,sol);
+            }else if(c==')') temp--;
+        }
+        return sol;
+    }
+};
