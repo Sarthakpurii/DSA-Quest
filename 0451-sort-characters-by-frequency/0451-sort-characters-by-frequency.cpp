9930c39b32de8eba53e8b8920fdf8de7bd1a9288
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-05-07 16:18:10 +0530

    Time: 8 ms (77.82%), Space: 11.2 MB (20.31%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        unordered_map<char, int> m;
+        for (char c : s) {
+            m[c]++;
+        }
+
+        multimap<int, char> mm;
+        for (auto c : m) {
+            mm.insert({c.second, c.first});
+        }
+        string sol = "";
+        for (auto it = mm.rbegin(); it != mm.rend(); it++) {
+            sol += string(it->first, it->second);
+        }
+        return sol;
+    }
+};
