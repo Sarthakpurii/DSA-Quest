e4ce2f4870dde1d11b29a6766331afaf6825c95d
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-05-20 23:42:31 +0530

    Time: 3 ms (92.3%), Space: 12.7 MB (40.08%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int sol=0,n=s.size();
+        unordered_map<char,int> conv={
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        
+        for(int i=0;i<n;i++){
+            if(s[i]=='I'){
+                if((i+1!=n) && (s[i+1]=='V' || s[i+1]=='X')) sol-=conv[s[i]];
+                else sol+=conv[s[i]];
+            }
+            else if(s[i]=='X'){
+                if((i+1!=n) && (s[i+1]=='L' || s[i+1]=='C')) sol-=conv[s[i]];
+                else sol+=conv[s[i]];
+            }
+            else if(s[i]=='C'){
+                if((i+1!=n) && (s[i+1]=='D' || s[i+1]=='M')) sol-=conv[s[i]];
+                else sol+=conv[s[i]];
+            }
+            else sol+=conv[s[i]];
+        }
+        return sol;
+    }
+};
