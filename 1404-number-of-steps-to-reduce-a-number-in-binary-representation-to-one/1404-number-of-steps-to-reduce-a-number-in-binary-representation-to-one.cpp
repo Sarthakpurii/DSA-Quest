f63fd030f56c92efeaeb92d228afb9177bbb17db
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-06-01 15:12:53 +0530

    Time: 12 ms (5.62%), Space: 10.3 MB (9.56%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -3,26 +3,7 @@ public:
     int numSteps(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        // int n=s.size(),ops=0,p=0;
-        // long int num=0;
-        // for(int i=n-1;i>-1;i--){
-        //     if(s[i]=='1'){
-        //         num+=pow(2,p);
-        //     }
-        //     p++;
-        // }
-        // cout<<num;
-        // while(num>1){
-        //     if(num%2==1){
-        //         num++;
-        //         ops++;
-        //     }
-        //     else{
-        //         num/=2;
-        //         ops++;
-        //     }
-        // }
-        // return ops;
+        
         int n=s.size()-1,ops=0;
         while(s!="1"){
             cout<<s<<endl<<n<<endl;
