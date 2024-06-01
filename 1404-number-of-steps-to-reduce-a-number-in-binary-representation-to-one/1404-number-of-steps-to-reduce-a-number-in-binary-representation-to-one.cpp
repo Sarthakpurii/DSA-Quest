6eb54bd008df5d4059ca24bb5aa3120626704e93
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-06-01 14:31:05 +0530

    Time: 17 ms (5.62%), Space: 10.5 MB (7.92%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -3,21 +3,49 @@ public:
     int numSteps(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        int n=s.size(),num=0,ops=0,p=0;
-        for(int i=n-1;i>-1;i--){
-            if(s[i]=='1'){
-                num+=pow(2,p);
-            }
-            p++;
-        }
-        while(num>1){
-            if(num%2==1){
-                num++;
+        // int n=s.size(),ops=0,p=0;
+        // long int num=0;
+        // for(int i=n-1;i>-1;i--){
+        //     if(s[i]=='1'){
+        //         num+=pow(2,p);
+        //     }
+        //     p++;
+        // }
+        // cout<<num;
+        // while(num>1){
+        //     if(num%2==1){
+        //         num++;
+        //         ops++;
+        //     }
+        //     else{
+        //         num/=2;
+        //         ops++;
+        //     }
+        // }
+        // return ops;
+        int n=s.size()-1,ops=0;
+        while(s!="1"){
+            cout<<s<<endl<<n<<endl;
+            if(s[n]=='0'){
                 ops++;
+                s=s.substr(0,n);
+                n--;
             }
             else{
-                num/=2;
                 ops++;
+                bool zeroEncounter=false;
+                for(int i=n;i>-1;i--){
+                    if(s[i]=='0'){
+                        s[i]='1';
+                        zeroEncounter=true;
+                        break;
+                    }
+                    else s[i]='0';
+                }
+                if(!zeroEncounter){
+                    s='1'+s;
+                    n++;
+                }
             }
         }
         return ops;
