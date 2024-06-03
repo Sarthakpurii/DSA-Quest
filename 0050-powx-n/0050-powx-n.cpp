1943f77cecc80da7e91f1f458f38af346d86ba0c
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-06-03 15:20:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // double powCalc(double x, int n){
+    //     if(n==1) return x;
+    //     double sol=powCalc(x,n-1)*x;
+    //     return sol;
+    // }
+    // double myPow(double x, int n) {
+    //     if (n==0) return 1;
+    //     bool neg=(n<0);
+    //     n=abs(n);
+    //     double sol;
+    //     sol=powCalc(x,n);
+    //     if (neg) return 1/sol;
+    //     return sol;
+    // }
+    double powCalc(double x,int n){
+        if(n==1) return x;
+        double sol;
+        if(n%2==1){
+            sol=x*powCalc(x,n-1);
+        }
+        else{
+            sol=powCalc(x*x,n/2);
+        }
+        return sol;
+        
+    }
+    double myPow(double x, int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if (n==0) return 1;
+        bool neg=(n<0);
+        n=abs(n);
+        double sol=powCalc(x, n);
+        if(neg) return 1/sol;
+        return sol;
+    }
+};
