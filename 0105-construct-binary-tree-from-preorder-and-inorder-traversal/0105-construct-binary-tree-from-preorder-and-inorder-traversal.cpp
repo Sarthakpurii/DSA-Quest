293ff59f819889c3e7fc2ed29e52452f939d82c7
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-07-13 20:54:35 +0530

    Time: 3 ms (99.16%), Space: 27.6 MB (13.7%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -17,24 +17,24 @@ public:
     //     }
     //     return -1;
     // }
-    TreeNode* build(const vector<int> preorder,const vector<int> inorder,int & root,pair<int,int> range){
+    TreeNode* build(const vector<int>& preorder,const vector<int>& inorder,int & root,pair<int,int> range,unordered_map<int,int>& mp){
         TreeNode* roo=new TreeNode(preorder[root]);
         if(range.first==range.second) return roo;
-        int idx;
-        for(int i=range.first;i<=range.second;i++){
-            if(inorder[i]==preorder[root]){
-                idx=i;
-                break;
-            } 
-        }
+        int idx=mp[preorder[root]];
+        // for(int i=range.first;i<=range.second;i++){
+        //     if(inorder[i]==preorder[root]){
+        //         idx=i;
+        //         break;
+        //     } 
+        // }
         if(range.first<=idx-1 && root<preorder.size()){
             root++;
-            TreeNode* left=build(preorder,inorder,root,{range.first,idx-1});
+            TreeNode* left=build(preorder,inorder,root,{range.first,idx-1},mp);
             roo->left=left;
         }
         if(range.second>=idx+1 && root<preorder.size()){
             root++;
-            TreeNode* right=build(preorder,inorder,root,{idx+1,range.second});
+            TreeNode* right=build(preorder,inorder,root,{idx+1,range.second},mp);
             roo->right=right;
         }
         return roo;
@@ -42,6 +42,9 @@ public:
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int root=0,n=preorder.size();
-        return build(preorder,inorder,root,{0,n-1});
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++) mp[inorder[i]]=i;
+        return build(preorder,inorder,root,{0,n-1},mp);
+
     }
 };
