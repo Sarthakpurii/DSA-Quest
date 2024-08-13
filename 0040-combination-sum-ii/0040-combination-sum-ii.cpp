d97a48464c39e30c22942d86c9c66491b6302c91
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-08-13 16:08:57 +0530

    Time: 5 ms (42.5%), Space: 12.9 MB (58.11%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void helper(vector<vector<int>>& ans,vector<int>& curr,vector<int>& cand,int remain, int idx){
+        if(remain==0){
+            ans.push_back(curr);
+            return;
+        }
+        if(remain<0 || idx==cand.size()) return;
+        for(int i=idx;i<cand.size();i++){
+            if(i!=idx && cand[i]==cand[i-1]) continue;
+            curr.push_back(cand[i]);
+            helper(ans,curr,cand,remain-cand[i],i+1);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& cand, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        sort(cand.begin(),cand.end());
+        // set<vector<int>> ans;
+        vector<int> curr;
+        vector<vector<int>>  ans;
+        helper(ans,curr,cand,target,0);
+        return ans;
+    }
+};
