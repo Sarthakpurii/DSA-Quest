d9850cf174ff12d6d9e5639b26602466852b04a6
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-08-27 05:44:47 +0530

    Time: 674 ms (5%), Space: 16.5 MB (68.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -19,7 +19,7 @@ public:
         for(int i=0;i<n;i++){
             dp[i][0]=true;
         }
-        dp[0][nums[0]]=true;
+        if(sum>=nums[0])dp[0][nums[0]]=true;
         for(int i=1;i<n;i++){
             for(int j=1;j<=sum;j++){
                 if(j>=nums[i]) dp[i][j]=dp[i][j] || dp[i-1][j-nums[i]];
