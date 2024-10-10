c914e2cf9b4370ffee6d5d69d083435268fb409a
Sarthak Puri <96935483+Sarthakpurii@users.noreply.github.com>
2024-10-10 21:03:24 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -3,6 +3,7 @@ public:
     int minSwaps(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        
         int currbalance=0,inbalance=0;
         for(char c:s){
             if(c=='[') currbalance--;
